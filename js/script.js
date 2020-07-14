7376e9f148d1aa5746ee61d8e33bc479a9c4fcd6
Roman <romariom4k@ya.ru>
2020-07-14 20:38:01 +0300

    Code refactoring

--- js/script.js
+++ js/script.js
@@ -1,97 +1,101 @@
 "use strict";
-let typeOfCode = 'num';
-let input = document.querySelector('#input');
-let output = document.querySelector('#output');
-let radio = document.querySelectorAll('.radio');
-console.log(radio);
 
-let radioHandler = (e) => {
+let typeOfCode = 'num';
+let langOfText = 'rus';
+const input = document.querySelector('#input');
+const output = document.querySelector('#output');
+const radioType = document.querySelectorAll('.radioType');
+const radioLang = document.querySelectorAll('.radioLang');
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+function isCharInRange(char) {
+    if (langOfText === 'rus') {
+        if (char.charCodeAt() > 1071 && char.charCodeAt() < 1104 ||
+            char.charCodeAt() === 1105) {
+            return true;
+        };
+    } else {
+        if (char.charCodeAt() > 96 && char.charCodeAt() < 123) {
+            return true;
+        };
+    };
+}
+
+function getChar(i) {
+    if (typeOfCode === 'num') {
+        return (i + 1);
+    } else if (langOfText === 'rus') {
+        return i !== 33 ? String.fromCharCode(i + 1072) : (String.fromCharCode(1105));
+    } else {
+        return String.fromCharCode(i + 97)
+    };
+}
+
+const radioHandler1 = (e) => {
     typeOfCode = e.target.value;
     inputHandler();
 };
 
-let inputHandler = (e) => {
+const radioHandler2 = (e) => {
+    langOfText = e.target.value;
+    console.log(langOfText);
+    inputHandler();
+};
+
+const inputHandler = (e) => {
+    const sourceText = input.value.toLowerCase();
+    let encryptChars = [];
+    let encryptKeys = {}; 
     let encryptedText = '';
-    let numbers = [];
-    let enigmaKeys = {};
-    let sourceChars = [];
 
-    let sourceText = input.value.toLowerCase();
-
-    for (let i = 1; i <= 33; i++) {
-        if(typeOfCode === 'num'){
-            numbers.push(i); 
-        } else {
-            numbers.push(String.fromCharCode(i + 1071)); 
+    for (let char of sourceText) {
+        if (isCharInRange(char) && !encryptKeys.hasOwnProperty(char)) {
+            encryptKeys[char] = ' ';
         };
     };
-    // console.log(enigmaKeys);
-    // console.log(sourceText[0].charCodeAt());
-    
-    function randomInteger(min, max) {
-        let rand = min + Math.random() * (max + 1 - min);
-        return Math.floor(rand);
-    }
-    
-    for (let i = 0; i < sourceText.length; i++) {
-        let charCode = sourceText[i].charCodeAt();
-    
-        if (charCode > 1071 && charCode < 1104 || charCode === 1105) {
-    
-            if (sourceChars.length === 0) {
-                sourceChars.push(sourceText[i]);
-                // console.log(sourceChars);
-    
-            } else {
-                let isUnique = true;
-                for (let k = 0; k < sourceChars.length; k++) {
-    
-                    if (sourceText[i] === sourceChars[k]) {
-                        isUnique = false;
-                    };
-                };
-                if (isUnique) {
-                    sourceChars.push(sourceText[i]);
-                    // console.log(sourceChars);
-                };
-            };
-        };
+
+    for (let i = 0; i < Object.keys(encryptKeys).length; i++) {
+            encryptChars.push(getChar(i));
     };
-    
-    numbers.splice(sourceChars.length, numbers.length - sourceChars.length);
-    
-    // console.log(numbers);
-    
-    for (let i = 0; i < sourceChars.length; i++) {
-        let randomNumber = randomInteger(0, numbers.length - 1);
-        enigmaKeys[sourceChars[i]] = numbers[randomNumber];
-        numbers.splice(randomNumber, 1);
+
+    for (let key in encryptKeys) {
+        let randomNumber = randomInteger(0, encryptChars.length - 1);
+        encryptKeys[key] = encryptChars[randomNumber];
+        encryptChars.splice(randomNumber, 1);
     };
-    
-    // console.log(enigmaKeys);
-    
+
     for (let i = 0; i < sourceText.length; i++) {
-        if(enigmaKeys[sourceText[i]]) {
-            // console.log(enigmaKeys[sourceText[i]]);
-            encryptedText += enigmaKeys[sourceText[i]];
-    
-            if (enigmaKeys[sourceText[i + 1]]){
+        if (encryptKeys[sourceText[i]]) {
+            encryptedText += encryptKeys[sourceText[i]];
+
+            if (encryptKeys[sourceText[i + 1]] && typeOfCode === 'num') {
                 encryptedText += '-';
             };
-    
+
         } else {
             encryptedText += sourceText[i];
-    
-            if(sourceText[i] == ' '){
+
+            //double space for easy reading
+            if (sourceText[i] == ' ' && typeOfCode === 'num') {
                 encryptedText += ' ';
             };
         };
     };
+    
     output.innerHTML = encryptedText;
 };
 
-input.addEventListener('input', inputHandler);
-
-for (let i = 0; i < radio.length; i++){
-    radio[i].addEventListener('input', radioHandler);
+// browser events
+for (let radio of radioType) {
+    radio.addEventListener('input', radioHandler1);
 };
+
+for (let radio of radioLang) {
+    radio.addEventListener('input', radioHandler2);
+};
+
+input.addEventListener('input', inputHandler);
